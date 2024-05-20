27c4cb458edc726498a54293ed81ab3702cea101
Gabriel <gabriel.mena.jofre@gmail.com>
2024-05-20 01:16:34 -0400

    Add .adonisrc.json configuration file 3

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const source_map_support_1 = __importDefault(require("source-map-support"));
 const standalone_1 = require("@adonisjs/core/build/standalone");
+const path = require("path");
+
 source_map_support_1.default.install({ handleUncaughtExceptions: false });
-new standalone_1.Ignitor(__dirname)
-    .httpServer()
-    .start();
-//# sourceMappingURL=server.js.map
+
+const appRoot = path.resolve(__dirname, ".");
+
+new standalone_1.Ignitor(appRoot).httpServer().start();
